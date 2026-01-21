b404f790cc0890c5ea191adf3302e3ee92ac6ca6
Gonzalo Mena - Menatech <gonzalo@menatech.cloud>
2026-01-21 18:33:52 -0400

    Replace incomplete Home.tsx file with the updated education section.

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,17 +1,42 @@
-const education = [
-  {
-    degree: "LL.M., International Studies, Distinction and Dean's List at Georgetown University Law Center, Washington D.C. (2008)",
-    logo: "/images/georgetown-logo-new.png",
-    url: "https://www.law.georgetown.edu",
-  },
-  {
-    degree: "Postgraduate Specialization in Business and Economics Law at Getúlio Vargas Foundation, São Paulo, Brazil (2006)",
-    logo: "/images/fgv-logo.png",
-    url: "https://portal.fgv.br",
-  },
-  {
-    degree: "LL.B. (J.D. Equivalent) at University of São Paulo Law School, São Paulo, Brazil (2000)",
-    logo: "/images/usp-logo.png",
-    url: "https://www.direito.usp.br",
-  },
-];
+import React from 'react';
+import { Container, Row, Col } from 'react-bootstrap';
+
+const Home = () => {
+  return (
+    <Container>
+      <Row>
+        <Col>
+          <h1>Education</h1>
+          <ul>
+            <li>
+              <strong>Georgetown University</strong> - 
+              LL.M., International Studies, Distinction and Dean's List
+              <br />
+              Washington, D.C. • 2021
+              <br />
+              <a href="https://www.georgetown.edu">Website</a>
+            </li>
+            <li>
+              <strong>FGV</strong> - 
+              Postgraduate Specialization in Business and Economics Law
+              <br />
+              São Paulo, Brazil • 2020
+              <br />
+              <a href="https://www.fgv.br">Website</a>
+            </li>
+            <li>
+              <strong>Universidade de São Paulo (USP)</strong> - 
+              LL.B. (J.D. Equivalent)
+              <br />
+              São Paulo, Brazil • 2018
+              <br />
+              <a href="https://www.usp.br">Website</a>
+            </li>
+          </ul>
+        </Col>
+      </Row>
+    </Container>
+  );
+};
+
+export default Home;
